acb935bc3b49a29c215e25783a9d076bb41834aa
Jim He <jim.he@brigade.com>
2017-07-21 09:31:43 -0700

    Add param to reviewboardid

--- zsh/linkedin.zsh
+++ zsh/linkedin.zsh
@@ -1,4 +1,9 @@
 reviewboardid () {
-  BRANCH=`git rev-parse --abbrev-ref head`
-  git config --local --get branch.$BRANCH.reviewid
+  if [ -n "$1" ]
+  then
+    git config --local --get branch."$1".reviewid
+  else
+    BRANCH=`git rev-parse --abbrev-ref head`
+    git config --local --get branch.$BRANCH.reviewid
+  fi
 }
